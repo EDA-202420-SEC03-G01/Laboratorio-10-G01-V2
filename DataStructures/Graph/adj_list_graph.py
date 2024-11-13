34c408947afde204d910404fb1edaf212c8a71a5
jota-y-ce <jc.bohorquezo1@uniandes.edu.co>
2024-11-13 17:02:07 -0500

    cambios 2

--- DataStructures/Graph/adj_list_graph.py
+++ DataStructures/Graph/adj_list_graph.py
@@ -1,5 +1,12 @@
-from DataStructures.Map import
+from DataStructures.Map import map_separate_chaining as sc
 
 def new_graph(size=19, directed=False):
     
-    graph = {"vertices": }
+    graph = {"vertices": sc.new_map(size, 0.5),
+             "infromation": sc.new_map(size, 0.5),
+             "in_degree": None,
+             "edges": 0,
+             "directed": directed,
+             "type": "ADJ_LIST"}
+    
+    return graph
